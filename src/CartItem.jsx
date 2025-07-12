a4edc559e1c45b22f80e8a7ecdc6da084b6a6833
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-07-12 18:15:00 +0530

    Checkout alert

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -87,7 +87,7 @@ const CartItem = ({ onContinueShopping }) => {
 					Continue Shopping
 				</button>
 				<br />
-				<button className="get-started-button1">Checkout</button>
+				<button className="get-started-button1" onClick={handleCheckoutShopping}>Checkout</button>
 			</div>
 		</div>
 	);
